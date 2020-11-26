07abdd6e6267f334929759271a77390b2e8b2442
Aldrighetti1 <75059160+Aldrighetti1@users.noreply.github.com>
2020-11-25 23:18:36 -0600

    Add files via upload

---
+++ ArchivoLista sin telefono/listaen/listaen/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace listaen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellidoPaterno = new System.Windows.Forms.TextBox();
+            this.txtApellidoMaterno = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.cmbMatricula = new System.Windows.Forms.ComboBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.rtbLista = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matricula:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apelldio Paterno:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Apellido Materno:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Direccion:";
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Location = new System.Drawing.Point(148, 59);
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(100, 20);
+            this.txtMatricula.TabIndex = 5;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(148, 107);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtApellidoPaterno
+            // 
+            this.txtApellidoPaterno.Location = new System.Drawing.Point(148, 153);
+            this.txtApellidoPaterno.Name = "txtApellidoPaterno";
+            this.txtApellidoPaterno.Size = new System.Drawing.Size(100, 20);
+            this.txtApellidoPaterno.TabIndex = 7;
+            // 
+            // txtApellidoMaterno
+            // 
+            this.txtApellidoMaterno.Location = new System.Drawing.Point(148, 191);
+            this.txtApellidoMaterno.Name = "txtApellidoMaterno";
+            this.txtApellidoMaterno.Size = new System.Drawing.Size(100, 20);
+            this.txtApellidoMaterno.TabIndex = 8;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(148, 235);
+            this.txtDireccion.Multiline = true;
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(180, 56);
+            this.txtDireccion.TabIndex = 9;
+            // 
+            // cmbMatricula
+            // 
+            this.cmbMatricula.FormattingEnabled = true;
+            this.cmbMatricula.Location = new System.Drawing.Point(281, 57);
+            this.cmbMatricula.Name = "cmbMatricula";
+            this.cmbMatricula.Size = new System.Drawing.Size(121, 21);
+            this.cmbMatricula.TabIndex = 10;
+            this.cmbMatricula.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(31, 320);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(148, 320);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 12;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(281, 320);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 15;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnlistar_Click);
+            // 
+            // rtbLista
+            // 
+            this.rtbLista.Location = new System.Drawing.Point(486, 36);
+            this.rtbLista.Name = "rtbLista";
+            this.rtbLista.Size = new System.Drawing.Size(230, 306);
+            this.rtbLista.TabIndex = 16;
+            this.rtbLista.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbLista);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.cmbMatricula);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtApellidoMaterno);
+            this.Controls.Add(this.txtApellidoPaterno);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtMatricula);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellidoPaterno;
+        private System.Windows.Forms.TextBox txtApellidoMaterno;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.ComboBox cmbMatricula;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.RichTextBox rtbLista;
+    }
+}
+
---
+++ ArchivoLista sin telefono/listaen/listaen/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listaen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Alumno> ListaAlumno;
+        Alumno alumno = new Alumno();
+        public void limpiar()
+        {
+            txtApellidoMaterno.Clear();
+            txtApellidoPaterno.Clear();
+            txtMatricula.Clear();
+            txtNombre.Clear();
+            txtDireccion.Clear();
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            limpiar();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                cargarcombo();
+            }
+            catch
+            {
+                MessageBox.Show("No existe el archivo");
+            }
+        }
+        public void cargarcombo()
+        {
+            alumno = new Alumno();
+            ListaAlumno = new List<Alumno>(alumno.LeerArchivo());
+            cmbMatricula.DataSource = ListaAlumno;
+            cmbMatricula.ValueMember = "matricula";
+            cmbMatricula.DisplayMember = "nombre";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlistar_Click(object sender, EventArgs e)
+        {
+            foreach(Alumno AlumnoImprime in ListaAlumno)
+            {
+                rtbLista.Text = rtbLista.Text + AlumnoImprime.Matricula + " " + AlumnoImprime.Nombre + " " +
+                    AlumnoImprime.ApellidoPaterno+" "+ AlumnoImprime.ApellidoMaterno+" "+ AlumnoImprime.Direccion+"\n";
+                    
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+            alumno = new Alumno(txtMatricula.Text, txtNombre.Text, txtApellidoPaterno.Text,txtApellidoMaterno.Text, txtDireccion.Text);
+            if (alumno.Guardar()==true)
+            {
+                cargarcombo();
+                limpiar();
+                MessageBox.Show("Registro Guardado");
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtMatricula.Text = cmbMatricula.SelectedValue.ToString();
+            txtNombre.Text =cmbMatricula.SelectedValue.ToString();
+            txtApellidoMaterno.Text = cmbMatricula.SelectedValue.ToString();
+            txtApellidoPaterno.Text = cmbMatricula.SelectedValue.ToString();
+            txtDireccion.Text = cmbMatricula.SelectedValue.ToString();
+        }
+
+        
+    }
+}
